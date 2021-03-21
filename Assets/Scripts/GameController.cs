773dc0dc03af69e1ef0c3b9d88b5636d3af3da7e
안단태 <48adt@naver.com>
2021-03-21 19:50:25 +0900

    temporary commit

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,4 @@
-﻿#define MOBILE
+﻿//#define MOBILE
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
--- Assets/Scripts/SwatMovement.cs
+++ Assets/Scripts/SwatMovement.cs
@@ -1,4 +1,4 @@
-﻿#define MOBILE
+﻿//#define MOBILE
 using System;
 using System.Collections;
 using System.Collections.Generic;
